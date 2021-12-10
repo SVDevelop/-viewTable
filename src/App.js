c43c058f373bc121e0313f731cdbdd87ee23efe0
svdevelop <verstka.sv@mail.ru>
2021-12-10 20:36:12 +0300

    fc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+import "./index.css";
+
+export default function App() {
+  const [t, setT] = useState()
+  const [val, setVal] = useState({
+    objects: [],
+    users: []
+  });
+  const desk = val.objects.filter(x => x.type === "desk") || [];
+  const pillar = val.objects.filter(x => x.type === "pillar") || [];
+  const table = val.objects.filter(x => x.type === "table") || [];
+  const users = val.users || [];
+  // object_id
+
+  useEffect(() => {
+    fetch("http://goodsok.ru/mock-api/users.php")
+      .then(results => results.json())
+      .then(res => {
+        console.log("приехали данные ", res);
+        setVal(data => ({
+          ...data,
+          users: res,
+        }));
+      })
+      .catch(e => console.log(e)); 
+
+    fetch("http://goodsok.ru/mock-api/objects.php")
+      .then(response => response.json())
+      .then(res => {
+        console.log(this);
+        console.log("приехали данные ", res);
+        setVal(data => ({
+          ...data,
+          objects: res,
+        }));
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{ positions: "relative", width: "100vw", height: "100vh" }}
+    >
+      {desk.length && <Desk data={desk} users={users} />}
+      {table.length && <Table data={table} users={users} />}
+      {pillar.length && <Pillar data={pillar} />}
     </div>
   );
 }
 
-export default App;
+function Desk({ data, users }) {
+  return data.map(({ id, type, left, top, angle }) => (
+    <div
+    key={id}
+      className="desk"
+      style={{
+        left: `${left}px`,
+        top: `${top}px`,
+        transform: `rotate(${angle}deg)`,
+      }}
+    >
+      {users.filter(x => x.object_id === id).map((x, key) => <User key={key} data={x} />)}
+    </div>
+  ));
+}
+
+function Table({ data, users }) {
+  return data.map(({ id, type, left, top, angle }) => (
+    <div
+    key={id}
+      className="table"
+      style={{
+        left: `${left}px`,
+        top: `${top}px`,
+        transform: `rotate(${angle}deg)`,
+      }}
+    >
+      {users.filter(x => x.object_id === id).map(x => <User data={x} />)}
+    </div>
+  ));
+}
+
+function Pillar({ data }) {
+  return data.map(({ id, type, left, top, angle }) => (
+    <div
+      className="pillar"
+      style={{
+        left: `${left}px`,
+        top: `${top}px`,
+        transform: `rotate(${angle}deg)`,
+      }}
+    >
+      {/* <div>{id}</div> */}
+    </div>
+  ));
+}
+
+function User({ data }) {
+  return <img src={data.avatar} />;
+}
